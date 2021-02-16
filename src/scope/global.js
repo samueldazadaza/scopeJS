e439a6c665dfac3633b357674c71ad237533a25e
Samuel Daza <samueldazadaza@gmail.com>
2021-02-15 23:43:21 -0500

    scope local

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,1 +1,30 @@
-var hello = 'HELLO'
+//CURSO 1
+var hello = 'HELLO';
+var hello = 'Hello ++'
+let world = 'hello world';
+let world = 'hello222';
+const helloWorld = 'hello World';
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+anotherFunction();
+
+//mala practica (no asignar nombre de variable)
+const helloWorld = () => {
+    globalVar = 'im global'
+}
+helloWorld();
+console.log(globalVar);
+
+//mala practica (se puede acceder desde afuera)
+const anotherFunction = () => {
+    var localVar = globalVar = 'Im global 33';
+};
+
+anotherFunction();
+console.log(globalVar);
+
+
